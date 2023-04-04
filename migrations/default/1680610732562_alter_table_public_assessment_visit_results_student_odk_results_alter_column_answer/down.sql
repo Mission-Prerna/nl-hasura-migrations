378dffd4771b79767b2a4589d3036668712d2941
yuvraj <yuvrajsab@protonmail.com>
2023-04-04 17:52:06 +0530

    change data type of assessment visit result tables

---
+++ migrations/default/1680610732562_alter_table_public_assessment_visit_results_student_odk_results_alter_column_answer/down.sql
@@ -0,0 +1,1 @@
+alter table "public"."assessment_visit_results_student_odk_results" alter column "answer" drop not null;
---
+++ migrations/default/1680610743980_alter_table_public_assessment_visit_results_student_odk_results_alter_column_question/up.sql
@@ -0,0 +1,1 @@
+alter table "public"."assessment_visit_results_student_odk_results" alter column "question" set not null;
