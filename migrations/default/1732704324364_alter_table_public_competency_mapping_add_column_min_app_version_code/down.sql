acce4bac2711e7b5be441e815ee1b36f5195445d
Ajay-Maury <ajaymaurya6798@gmail.com>
2024-12-02 14:15:19 +0530

    fix: Added min_app_version_code in competency_mapping table

---
+++ migrations/default/1732704324364_alter_table_public_competency_mapping_add_column_min_app_version_code/down.sql
@@ -0,0 +1,4 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."competency_mapping" add column "min_app_version_code" integer
+--  not null default '0';
---
+++ migrations/default/1732704324364_alter_table_public_competency_mapping_add_column_min_app_version_code/up.sql
@@ -0,0 +1,2 @@
+alter table "public"."competency_mapping" add column "min_app_version_code" integer
+ not null default '0';
